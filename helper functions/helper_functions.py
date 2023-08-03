402215cacf31d421afa92e2197ba980e0a98abc0
LuckyMS <114200423+LuckyMS@users.noreply.github.com>
2023-08-02 20:30:34 -0500

    add helper functions

---
+++ helper functions/helper_functions.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchaudio
+import sys
+
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+
+from tqdm import tqdm
+
+import os
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import os
+import glob
+import sklearn
+from torch.utils.tensorboard import SummaryWriter
+import random
+
+##################################################################################################
+# First things first! Set a seed for reproducibility.
+# https://www.cs.mcgill.ca/~ksinha4/practices_for_reproducibility/
+def set_seed(seed):
+    """Set seed"""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed)
